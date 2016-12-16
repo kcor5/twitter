5f3d7e523a0aa2b61851ce38425e3e9cab141f59
Kevin Corliss <corliss.kevin@gmail.com>
2016-12-15 19:58:29 -0500

    created tweets array and friends hash

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,10 @@
 class TweetsController < ApplicationController
 	def new
+		@tweets = ["This is cool", "I'm learning how to code", "Very beneficial"]
+		@friends = {
+			Jody: "cool",
+			Ethan: "owes me money",
+			Tricia: "likes to dance"
+		}
 	end
 end
