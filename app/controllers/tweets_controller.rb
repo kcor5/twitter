3bfc36e0ed59ef80e78507edba6ea40635c1e9e8
Kevin Corliss <corliss.kevin@gmail.com>
2016-12-19 20:09:44 -0500

    action create made in tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,7 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+	def create
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'tweets/new', to: 'tweets#new'
+  # get 'tweets', to: 'tweets#index'
+  # get 'tweets/:tweet_id', to: 'tweets/show'
+
   resources :tweets
+  
 end
