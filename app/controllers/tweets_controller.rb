57ad1e47b5c54afb81f471d91838414c1f0cc45a
Kevin Corliss <corliss.kevin@gmail.com>
2016-12-19 20:34:03 -0500

    tried to save tweet from form but says not permitted

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,10 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+
 	def create
-		
+		puts params[:tweet].inspect
+		@tweet = Tweet.new(params[:tweet])
+		@tweet.save
 	end
 end
