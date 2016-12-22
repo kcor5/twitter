96584382be2c0450c1f51f8cdd606de9597952fb
Philip Greenwald <philip@avidest.com>
2016-12-22 17:38:28 -0500

    Added a flash message that shows in new.html.erb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,18 @@
 class TweetsController < ApplicationController
 	def new
+		puts "flash is #{flash.inspect}"
 		@tweet = Tweet.new
 	end
 
 	def create
 		puts params[:tweet].inspect
 		@tweet = Tweet.new(tweet_params)
-		@tweet.save
+		if @tweet.save
+			flash[:success] = "You have created a tweet."
+		else
+			flash[:error] = "Tweet failed"
+		end
+		# flash = {success: 'you have created a tweet'}
 		redirect_to new_tweet_path
 	end
 
